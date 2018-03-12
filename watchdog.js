350a4ad5e36ac31a8dcc05e75380843ec8a6ee6e
kosovsky <nadav.kosovsky@mavenir.com>
2018-03-12 08:38:47 +0200

    require prefUtils before TestMgr

--- watchdog.js
+++ watchdog.js
@@ -1,6 +1,6 @@
-var TestMgr = require("./TestMgr");
 global.EventBus = require("./EventBus");
 global.Utils = require("./Utils");
+global.PrefsUtils=require("./PrefsUtils");
 global.command={};
 for(var i = 2 ; i < process.argv.length ; i++){
     //console.log(i + ': ' + process.argv[i]);
@@ -8,4 +8,5 @@ for(var i = 2 ; i < process.argv.length ; i++){
         global.command[process.argv[i].slice(2)]  =  process.argv[i+1]
     }
 }
+var TestMgr = require("./TestMgr");
 TestMgr.init();
